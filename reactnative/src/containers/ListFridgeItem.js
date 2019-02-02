4eee170277dbb36f4b8c45a97d3b6433b94f8eed
Nigel Lee <lee.nigel@hotmail.com>
2019-02-02 21:44:29 +0800

    delete button

--- reactnative/src/containers/ListFridgeItem.js
+++ reactnative/src/containers/ListFridgeItem.js
@@ -1,9 +1,21 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import Swipeout from 'react-native-swipeout';
 import { CardSection } from '../common';
 
+import fridgeFoodDelete from '../actions';
+
+import { connect } from 'react-redux';
+
 class ListFridgeItem extends Component {
+   
+    onButtonPress() {
+       const { uid } = this.props.fridgeFoods;
+       console.log(uid);
+       this.props.employeeDelete({ uid });
+    }
+   
     render() {
         const { name, expiry } = this.props.fridgeFood;
 
@@ -11,7 +23,7 @@ class ListFridgeItem extends Component {
             text: 'Eaten',
             backgroundColor: 'blue',
             underlayColor: 'rgba(0, 0, 0, 1, 0.6)',
-            onPress: ()=> { }
+            onPress: () => {this.onButtonPress.bind(this)}
           },
           {
             text: 'Disposed',
@@ -53,4 +65,12 @@ const styles = {
     }
 };
 
-export default ListFridgeItem;
+const mapStateToProps = (state) => {
+    const fridgeFoods = _.map(state.fridgeFoods, (val, uid) => {
+        return { ...val, uid };
+    });
+
+    return { fridgeFoods };
+};
+
+export default connect(mapStateToProps, { fridgeFoodDelete })(ListFridgeItem);
