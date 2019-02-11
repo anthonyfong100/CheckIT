07cb85f976b7c6f9a03262dd95f894cf1421633d
nigel lee <lee.nigel@hotmail.com>
2019-02-11 23:32:48 +0800

    Merged with text detect

--- reactnative/android/app/src/main/java/com/reactnative/MainApplication.java
+++ reactnative/android/app/src/main/java/com/reactnative/MainApplication.java
@@ -3,6 +3,12 @@ package com.reactnative;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.fetchsky.RNTextDetector.RNTextDetectorPackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import io.invertase.firebase.RNFirebasePackage;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import org.reactnative.camera.RNCameraPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
 import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
@@ -46,14 +52,14 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNFirebasePackage(),
             new VectorIconsPackage(),
             new RNGestureHandlerPackage(),
+            new RNFirebasePackage(),
             new RNCameraPackage(),
             new FBSDKPackage(mCallbackManager),
             new RNFirebaseMessagingPackage(),
             new RNFirebaseNotificationsPackage(),
-            new RNTextDetectorPackage(),
+            new RNTextDetectorPackage()
       );
     }
 
