e6daf839f246041cecf8a6e45d4383a8dfc37b88
nigel lee <lee.nigel@hotmail.com>
2019-02-11 21:08:04 +0800

    add modules

--- reactnative/android/app/src/main/java/com/reactnative/MainApplication.java
+++ reactnative/android/app/src/main/java/com/reactnative/MainApplication.java
@@ -3,6 +3,7 @@ package com.reactnative;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.fetchsky.RNTextDetector.RNTextDetectorPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -41,6 +42,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNTextDetectorPackage(),
             new VectorIconsPackage(),
             new RNGestureHandlerPackage(),
             new RNCameraPackage(),
